a2e1a4e23ffcd06002f6ecc9d2206c1fc0fac985
penguinite <penguinite@tuta.io>
2024-11-12 12:15:58 +0100

    wip: Tokenizer finished
    Now onto the parser...
    
    UGHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- src/temple.nim
+++ src/temple.nim
@@ -1,7 +1,13 @@
 import std/[json, strutils]
 
-const data = """
-Hello $name$!
+const safeMode = false
+
+const data = %* {
+  "name": "John"
+}
+
+const input = """
+Hello \$name\$!
 """
 
 # First pass. Converting the input data into a basic set of tokens.
@@ -10,7 +16,6 @@ Hello $name$!
 type
   TokenKind = enum
     Block, # A static block of text.
-    DollarSign, # Command start/end symbol
     Colon, # Used for attributes.
     Symbol, # A command.
   
@@ -26,11 +31,20 @@ var
   backslash, cmdMode = false
   tmp = ""
 
-for ch in data:
+for ch in input:
+  if backslash:
+    tmp.add(ch)
+    backslash = false
+    continue
+
   case cmdMode:
   of true:
     case ch:
     of '$':
+      # Check if tmp already has data stored, and if so then add it to the tree.
+      if not tmp.isEmptyOrWhitespace():
+        tokenTree.add(Token(kind: Symbol, inner: tmp))
+        tmp = ""
       # Just disable command mode...
       cmdMode = false
     of ':':
@@ -47,13 +61,30 @@ for ch in data:
       if not tmp.isEmptyOrWhitespace():
         tokenTree.add(Token(kind: Symbol, inner: tmp))
         tmp = ""
+    of '\\': backslash = true # Basic backslash handling
     else:
-      # Just 
+      # Just add whatever we find to the tmp var.
+      # It'll be sorted out eventually...
       tmp.add(ch)
-
-  of false
+  of false:
     case ch:
     of '$':
+      # Check if tmp has something in it
+      # if it does then add it to the tree
+      if not tmp.isEmptyOrWhitespace():
+        tokenTree.add(Token(kind: Block, inner: tmp))
+        tmp = ""
+      cmdMode = true
+    of '\\': backslash = true # Basic backslash handling
+    else:
+      tmp.add(ch)
+      discard
+
+# One last check to see if tmp has data in it.
+if not tmp.isEmptyOrWhitespace():
+  case cmdMode
+  of true: tokenTree.add(Token(kind: Symbol, inner: tmp))
+  of false: tokenTree.add(Token(kind: Block, inner: tmp))
 
 when defined(templeDebug):
   for token in tokenTree:
@@ -61,7 +92,34 @@ when defined(templeDebug):
     case token.kind:
     of Block, Symbol:
       echo "inner: ", token.inner
+    else: discard
 
+var
+  output = ""
+  i = -1
+
+proc getUntilEnd*(tree: seq[tokenTree], pointer: int): seq[TokenTree] =
+
+  
+for token in tokenTree:
+  inc i
+  case token.kind:
+  of Block:
+    output.add(token.inner)
+  of Symbol:
+    case token.inner
+    of "if", "for", "end": continue
+    else:
+      # Simple key
+      if "." notin token.inner:
+        continue
+
+      # If the key is an "Array" then it must be
+      # accessible through loops.
+      if 
+
+      # Advanced "Object" key
+      if data.hasKey()
 
 
 proc templateify*(input: string, data: JsonNode): string =
